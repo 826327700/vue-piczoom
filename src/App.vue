d4ec16d3e21390f8af6081ea9a90be3d7c79cf58
xiaobaozi <826327700@qq.com>
2018-01-20 15:48:47 +0800

    优化多个实例共存

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+      <p>配置3倍放大：<span style="color:#ff6600">:scale="3"</span></p>
       <div class="pic-box">
-        <pic-zoom url="static/imac2.jpg" big-url="static/imac2.jpg" :scale="3"></pic-zoom>
+        <pic-zoom url="static/imac2.jpg" :scale="3"></pic-zoom>
+      </div>
+      <p>配置放大时可滚动页面(滚轮)：<span style="color:#ff6600">:scroll="true"</span></p>
+      <div class="pic-box">
+        <pic-zoom url="static/imac2.jpg" :scale="3" :scroll="true"></pic-zoom>
+      </div>
+      <p>分开配置大小图片地址：<span style="color:#ff6600">url="static/imac2_thumb.jpg" big-url="static/imac2.jpg"</span></p>
+      <div class="pic-box">
+        <pic-zoom url="static/imac2_thumb.jpg" big-url="static/imac2.jpg" :scale="3" ></pic-zoom>
       </div>
   </div>
 </template>
@@ -22,7 +31,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/PicZoom.vue
+++ src/components/PicZoom.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="magnifier-box" @mousemove="mousemove" @mouseover="mouseover" @mouseleave="mouseleave">
+    <div class="magnifier-box" :ref="id" @mousemove="mousemove" @mouseover="mouseover" @mouseleave="mouseleave">
         <img :src="url" alt="">
         <div class="mouse-cover"></div>
     </div>
@@ -27,6 +27,7 @@
         },
         data () {
             return {
+                id:null,
                 cover:null,
                 imgbox:null,
                 imgwrap:null,
@@ -37,46 +38,69 @@
                 rectTimesY:0,
                 imgTimesX:0,
                 imgTimesY:0,
+                init:false
             }
+        },
+        created(){
+        　　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+        　　var maxPos = $chars.length;
+        　　var str = '';
+        　　for (let i = 0; i < 10; i++) {
+                str += $chars.charAt(Math.floor(Math.random() * maxPos));
+        　　}
+            this.id=str
         },
         mounted(){
-            this.cover=document.querySelector('.mouse-cover')
-            this.imgbox=document.querySelector('.magnifier-box')
-            this.cover.style.width=(this.imgbox.offsetWidth/this.scale)+'px'
-            this.cover.style.height=(this.imgbox.offsetHeight/this.scale)+'px'
-            this.imgwrap=document.querySelector('.magnifier-box img')
-            let imgsrc;
-            if(this.bigUrl){
-                imgsrc=this.bigUrl
-            }else{
-                imgsrc=document.querySelector('.magnifier-box img').src
-            }
-            this.img=new Image()
-            this.img.src=imgsrc
-            this.img.onload=()=>{
-                this.rectTimesX=this.cover.offsetWidth/this.imgwrap.offsetWidth,
-                this.rectTimesY=this.cover.offsetHeight/this.imgwrap.offsetHeight
-
-                this.imgTimesX=this.img.width/this.imgwrap.offsetWidth,
-                this.imgTimesY=this.img.height/this.imgwrap.offsetHeight
-            }
-
-            if(!this.canvas){
-                this.canvas=document.createElement('canvas')
-                this.canvas.className='mouse-cover-canvas'
-                this.canvas.style.position='absolute'
-                this.canvas.style.left=this.imgbox.offsetLeft+this.imgbox.offsetWidth+10+'px'
-                this.canvas.style.top=this.imgbox.offsetTop+'px'
-                this.canvas.style.border='1px solid #eee'
-                this.canvas.style.zIndex='99999'
-                this.canvas.height=this.imgbox.offsetHeight
-                this.canvas.width=this.imgbox.offsetWidth
-                document.body.append(this.canvas)
-            }
-            this.ctx=this.canvas.getContext("2d");
+            this.$nextTick(()=>{
+                this.initBox()
+            })
         },
         methods: {
+            initBox(){
+                let box=this.$refs[this.id]
+                this.imgbox=box
+                this.cover=box.querySelector('.mouse-cover')
+                this.cover.style.width=(this.imgbox.offsetWidth/this.scale)+'px'
+                this.cover.style.height=(this.imgbox.offsetHeight/this.scale)+'px'
+                this.cover.style.left='-100%'
+                this.cover.style.top='-100%'
+                this.imgwrap=box.querySelector('img')
+                let imgsrc;
+                if(this.bigUrl){
+                    imgsrc=this.bigUrl
+                }else{
+                    imgsrc=this.imgwrap.src
+                }
+                this.img=new Image()
+                this.img.src=imgsrc
+                this.img.onload=()=>{
+                    this.rectTimesX=this.cover.offsetWidth/this.imgwrap.offsetWidth,
+                    this.rectTimesY=this.cover.offsetHeight/this.imgwrap.offsetHeight
+
+                    this.imgTimesX=this.img.width/this.imgwrap.offsetWidth,
+                    this.imgTimesY=this.img.height/this.imgwrap.offsetHeight
+                }
+
+                if(!this.canvas){
+                    this.canvas=document.createElement('canvas')
+                    this.canvas.className='mouse-cover-canvas'
+                    this.canvas.style.position='absolute'
+                    this.canvas.style.left=this.imgbox.offsetLeft+this.imgbox.offsetWidth+10+'px'
+                    this.canvas.style.top=this.imgbox.offsetTop+'px'
+                    this.canvas.style.border='1px solid #eee'
+                    this.canvas.style.zIndex='99999'
+                    this.canvas.height=this.imgbox.offsetHeight
+                    this.canvas.width=this.imgbox.offsetWidth
+                    this.canvas.style.display='none'
+                    document.body.append(this.canvas)
+                }
+                this.ctx=this.canvas.getContext("2d");
+                this.init=true
+            },
             mousemove(e){
+                if(!this.init){
+                    return false
+                }
                 let _this=this
                 
                 function getXY(eve) {
@@ -92,7 +116,7 @@
                     this.cover.style.top=pos.y+'px'
                 }
                 if(pos.x<=this.imgwrap.offsetLeft+this.imgwrap.offsetWidth-this.cover.offsetWidth&&pos.x>=this.imgwrap.offsetLeft&&pos.y<=this.imgwrap.offsetTop+this.imgwrap.offsetHeight-this.cover.offsetHeight&&pos.y>=this.imgwrap.offsetTop){
-                    
+
                     this.ctx.clearRect(0,0,this.imgwrap.offsetWidth,this.imgwrap.offsetHeight);
                     let startX=this.cover.offsetLeft-this.imgwrap.offsetLeft,
                     startY=this.cover.offsetTop-this.imgwrap.offsetTop
@@ -101,6 +125,9 @@
                 
             },
             mouseover(e){
+                if(!this.init){
+                    return false
+                }
                 e=e||event
                 if(!this.scroll){
                     e.currentTarget.addEventListener("mousewheel",function(ev){
@@ -111,11 +138,14 @@
                         ev.preventDefault();
                     },false);
                 }
-
+                
                 this.cover.style.display='block'
                 this.canvas.style.display='block'
             },
             mouseleave(){
+                if(!this.init){
+                    return false
+                }
                 this.cover.style.display='none'
                 this.canvas.style.display='none'
             }
